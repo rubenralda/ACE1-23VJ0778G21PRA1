9513066002b55d18cb5cb01d9368640515b7412b
andre-corado <3007947540101@ingenieria.usac.edu.gt>
2023-06-08 18:21:33 -0600

    Merge Stats on Main

--- Practica1/Practica1.ino
+++ Practica1/Practica1.ino
@@ -15,6 +15,66 @@
 #define BTN_K 3
 LedControl matrix_driver = LedControl(51, 53, 52, 1);
 
+
+// ------------------------------------------------------
+// -------------------- ESTADÍSTICAS --------------------
+int punteos[] = {0,0,0,0,0}; // Stack de Punteos
+int stats[] = {0,0,0,0,0}; // Stack de Stats (0-8)
+byte bufferStats[8][16];
+
+
+void limpiarBufferStats(){
+  for (int row = 0; row < 8; row++) {
+    for (int col = 0; col < 16; col++) {
+      bufferStats[row][col] = 0;
+    }
+  }
+}
+
+void calcularStat(int indiceStat, int sumatoria){
+  if (sumatoria == 0){
+    stats[indiceStat] = 1;
+    return;
+  }
+  float porcentaje = (float(punteos[indiceStat]) / sumatoria * 100);
+  int stat = map(porcentaje, 0, 100, 1, 8); // Mapear, valor, 0-100 porcentaje, 1-8 valor barra en matriz
+  stats[indiceStat] = stat;
+}
+
+void generarStats(){
+  int sumatoria = punteos[0] + punteos[1] + punteos[2] + punteos[3] + punteos[4];
+  for (int i = 0; i <= 4; i++){ // Por cada punteo
+    calcularStat(i, sumatoria); // Cálculo de estadística 0-8
+  }
+}
+
+void agregarPunteo(int nuevoPunteo){ // Añade al Stack de Punteos
+  for (int i = 4; i <= 1; i--){ // Por cada punteo
+    punteos[i] = punteos[i-1];  // Se corre una posición
+  }
+  punteos[0] = nuevoPunteo;     // Se agrega nuevo punteo
+  generarStats();               // Genera las estadísticas del 0-8
+}
+
+void actualizarBufferStats(){
+  int col = 1;
+  for(int stat = 0; stat < 5; stat++){ // Por cada Stat
+    for(int i=0; i<2; i++){ // 2 Veces (2 columnas)
+      if(stats[stat] != 0){
+        for (int row = 7; row > (7 - stats[stat]); row--) {       
+          bufferStats[row][col] = 1;      
+        }
+      }
+      col++;
+    }
+    col++;
+  }
+}
+// -------------------- ESTADÍSTICAS --------------------
+// ------------------------------------------------------
+
+
+
 //*************MENSAJE ----- MENU****************
 int contMenu=0;
 
@@ -397,7 +457,7 @@ void newgame(){
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  randomSeed(analogRead(0));
+  randomSeed(analogRead(0)); 
 
   // Definicion de los pines de salida para las filas y columnas
   for (int i = 0; i < 8; i++) {
@@ -417,6 +477,12 @@ void setup() {
   pinMode(LEFT, INPUT);
   pinMode(START, INPUT);
   pinMode(BTN_K, INPUT);
+
+ //Inicialización de Estadísticas
+  generarStats();
+  limpiarBufferStats();
+  actualizarBufferStats();
+
 }
 
 void loop() {
